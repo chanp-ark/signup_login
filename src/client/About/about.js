b4af515bf045fde82424c58267eb2c911419ffcd
Chan Park <chanpark93@gmail.com>
2020-03-21 14:36:43 -0700

    finish about, start projects

--- src/client/About/about.js
+++ src/client/About/about.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import "./about.css"
+
 const About = () => {
     return (
-        <h1>ABOUT PAGE</h1>
+        <div className="about-container">
+            <div className="about-title">Project Together</div>
+            <div className="about-motto">Going Further Faster Together</div>
+            <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" />
+            <div className="about-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div className="about-description">
+                Lacus sed viverra tellus in hac habitasse. Sed turpis tincidunt id aliquet. Laoreet suspendisse interdum consectetur libero. Hac habitasse platea dictumst vestibulum rhoncus est. Nulla facilisi etiam dignissim diam quis enim lobortis. Quis eleifend quam adipiscing vitae proin sagittis. Ultrices neque ornare aenean euismod elementum nisi quis eleifend. Hendrerit gravida rutrum quisque non tellus orci. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Tincidunt ornare massa eget egestas purus viverra accumsan. Sed cras ornare arcu dui vivamus. Suscipit tellus mauris a diam maecenas sed enim. Posuere sollicitudin aliquam ultrices sagittis orci a. Libero id faucibus nisl tincidunt.
+            </div>
+
+        </div>
     )
 }
 
--- src/client/Projects/projects.js
+++ src/client/Projects/projects.js
@@ -1,6 +1,46 @@
 import React from 'react'
 
 const Projects = () => {
+    
+    // make thumbnails of projects, 2 columns 
+    // fill initialState with projects in database
+    const initialState = [
+        {
+            name: "Project 1",
+            tech_stack: "react, node",
+            curCap: 2,
+            maxCap: 6,
+            users: ["John", "Jane"]
+        },
+        {
+            name: "Project 2",
+            tech_stack: "angular, express",
+            curCap: 4,
+            maxCap: 5,
+            users: ["James", "Jo", "Joseph", "Jasmine"]
+        },
+        {
+            name: "Project 3",
+            tech_stack: "jQuery, PHP",
+            curCap: 1,
+            maxCap: 3,
+            users: ["Diane"]
+        },
+        {
+            name: "Project 4",
+            tech_stack: "react, node",
+            curCap: 2,
+            maxCap: 4,
+            users: ["Kenzie", "Kash"]
+        }
+    ]
+    
+    const [projects, setProjects] = React.useState(initialState)
+    
+    const [curProj, setCurProj] = React.useState(projects[0])
+    
+    const {name, tech_stack, curCap, maxCap, users} = curProj
+    
     return (
         <h1>PROJECTS</h1>
     )
